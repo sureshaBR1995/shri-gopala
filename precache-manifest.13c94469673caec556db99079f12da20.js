4f79b9a0abc45c110d81116ad057699f2c964791
sureshaBR1995 <sureshblk95@gmail.com>
2020-02-13 13:25:32 +0530

    Updates

---
+++ precache-manifest.13c94469673caec556db99079f12da20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed5f3bd1ee5037958608ef1379b6d98",
+    "url": "/shri-gopala/index.html"
+  },
+  {
+    "revision": "865aed5a2f0960678c1a",
+    "url": "/shri-gopala/static/css/main.a614ba6f.chunk.css"
+  },
+  {
+    "revision": "e80623c3cc68f391338b",
+    "url": "/shri-gopala/static/js/2.964a0902.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shri-gopala/static/js/2.964a0902.chunk.js.LICENSE"
+  },
+  {
+    "revision": "865aed5a2f0960678c1a",
+    "url": "/shri-gopala/static/js/main.6df10cba.chunk.js"
+  },
+  {
+    "revision": "68163dde5c0a8a7b11c3",
+    "url": "/shri-gopala/static/js/runtime-main.30a41e70.js"
+  }
+]);
